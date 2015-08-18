96dab32b4abe3aee6136e115c9caf3d4bf75e17d
stevejamesson <steve@evendev.com>
2015-08-18 15:21:55 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var argv = require('yargs').argv;
+var harp = require('harp');
+var rsync = require('rsyncwrapper').rsync;
+var util = require('util');
+
+
+global.moment = require('moment');
+global._ = require('underscore');
+
+
+var build = function() {
+  util.log('Compiling...');
+  harp.compile('.', './www', function(err) {
+    if (err) {
+      util.log('Compile failed with errors:');
+      util.log(err);
+    } else {
+      util.log('Compile completed without errors');
+    }
+  });
+};
+
+var serve = function() {
+  var port = argv.port || '9000';
+  console.log('Serving on port 9000');
+  harp.server('.', {
+    port: port
+  });
+};
+
+var deploy = function() {
+
+};
+
+
+switch (argv._[0]) {
+  case 'serve':
+    serve();
+    break;
+  case 'build':
+    build();
+    break;
+  default:
+    util.log('No command specified.');
+    break;
+}
